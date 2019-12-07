a58cc39663a34f51ad3c3186b96fd0cd75f957cf
xandjiji <xandjiji@gmail.com>
2019-12-07 11:09:51 -0300

    initial pattern

--- sketch.js
+++ sketch.js
@@ -1,7 +1,54 @@
+var canvasWidth = 600;
+var canvasHeight = 600;
+
+var initialPattern = generatePattern();
+
 function setup() {
-  // put setup code here
+    var canvas = createCanvas(canvasWidth, canvasHeight);
+
+    canvas.position(0, 100);
+    canvas.center('horizontal')
 }
 
 function draw() {
-  // put drawing code here
+    background(255, 204, 100);
+    
+
+    drawPattern();
+}
+
+function drawPattern() {
+    for(let i = 0; i < initialPattern.length; i++) {
+        rect(initialPattern[i].x, initialPattern[i].y, 1, 1);
+    }
+}
+
+function generatePattern() {
+    let x = canvasWidth / 2;
+    let y = canvasHeight / 2;
+    let pattern = [];
+
+    pattern.push({x, y});
+
+    for(let i = 0; i < 9; i++) {
+
+        let lastElement = pattern[pattern.length - 1];
+        let mutant = mutate(lastElement);
+        pattern.push(mutant);
+    }
+
+    return pattern;
+}
+
+
+function rng() {
+    return Math.round(Math.random() * 2) -1;
+}
+
+function mutate(input) {
+
+    let x = input.x + rng();
+    let y = input.y + rng();
+
+    return {x, y};
 }
